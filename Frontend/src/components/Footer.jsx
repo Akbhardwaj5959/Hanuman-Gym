bae50742db463a8c122651531c625a13929fce9c
Akbhardwaj5959 <riyajay31@gmail.com>
2024-07-07 20:54:48 +0530

    final

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      © 2024 Hanuman gym, Inc
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>HANUMAN GYM</p>
+    </header>
+  )
+}
+
+export default Navbar
